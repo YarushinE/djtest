77ce53b1d4b52ef1c6315dd17dc9dd9b68f6523e
Evgeniy <yarushin_72@mail.ru>
2022-01-23 00:55:25 +0500

    lesson#3

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from women.views import index
+from women.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('women/', index),
+    path('cats/', categories),
 ]

0424edf3983ec283fba70a1312c7341f49e03e22
Evgeniy <yarushin_72@mail.ru>
2022-02-12 17:43:24 +0500

    lesson#344

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns=[
     path('',index),
+    path('about/', about,name='about'),
     path('cats/<int:catid>/',categories),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
